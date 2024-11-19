abbfd7d862ec844ce5c63284ca5bbdebf5816780
Nolan Kyhl <nolan@kyhl.com>
2024-11-19 00:23:41 -0500

    prepare and upload dataset script

---
+++ generate-simple-uis/prepare_and_upload_dataset.py
@@ -0,0 +1,36 @@
+import os
+import json
+from datasets import Dataset, Features, Image, Value, DatasetDict
+from huggingface_hub import login
+from ast import literal_eval
+
+login()
+image_dir = 'screenshots/complete'
+label_dir = 'labels/complete'
+
+data = []
+for image_name in os.listdir(image_dir):
+    base = os.path.splitext(image_name)[0]
+    image_path = os.path.join(image_dir, image_name)
+    caption_path = os.path.join(label_dir, f'{base}.caption')
+    config_path = os.path.join(label_dir, f'{base}.config')
+
+    with open(caption_path, 'r') as f:
+        caption = f.read().strip()
+
+    with open(config_path, 'r') as f:
+        d = f.read().strip()
+        config = literal_eval(d)
+        config = json.dumps(config)
+
+    data.append({"image": image_path, "name": base, "caption": caption, "config": config})
+
+features = Features({
+    'image': Image(),
+    'name': Value('string'),
+    'caption': Value('string'),
+    'config': Value('string')
+})
+
+dataset = Dataset.from_list(data, features=features)
+dataset.push_to_hub("nkyhl/simple-uis")
